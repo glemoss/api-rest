d2774d0c282efc5dfbcea2813a72accb3e539dd8
Gabriel Lemos <glemoss@yahoo.com>
2024-07-03 10:05:37 -0300

    test: get transaction by id and get transaction summary

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -27,6 +27,37 @@ describe('Transactions routes', () => {
             }).expect(201)
     })
 
+    it('should be able to get a transaction by its id', async () => {
+        const createTransactionResponse = await supertest(app.server)
+            .post('/transactions')
+            .send({
+                title: 'test get transaction by id',
+                amount: 10,
+                type: 'credit'
+            })
+
+        const cookies = createTransactionResponse.get('Set-Cookie')
+
+        const transactionList = await supertest(app.server)
+            .get('/transactions')
+           .set('Cookie', cookies!)
+           .expect(200)
+
+        const transactionId = transactionList.body.transactions[0].id
+
+        const getTransactionById = await supertest(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set('Cookie', cookies!)
+            .expect(200)
+
+        expect(getTransactionById.body.transaction).toEqual(
+            expect.objectContaining({
+                title: 'test get transaction by id',
+                amount: 10,
+            })
+        )
+    })
+
     it('should be able to list all transactions', async () => {
         const createTransactionResponse = await supertest(app.server)
             .post('/transactions')
@@ -47,6 +78,38 @@ describe('Transactions routes', () => {
             expect.objectContaining({
                 title: 'test create transaction',
                 amount: 10,
+            })
+        ])
+    })
+
+    it('should be able to get transactions summary', async () => {
+        const creditTransaction = await supertest(app.server)
+            .post('/transactions')
+            .send({
+                title: 'credit transaction',
+                amount: 10,
+                type: 'credit'
+            })
+
+        const cookies = creditTransaction.get('Set-Cookie')
+
+        const debitTransaction = await supertest(app.server)
+            .post('/transactions')
+            .set('Cookie', cookies!)
+            .send({
+                title: 'debit transaction',
+                amount: 10,
+                type: 'debit'
+            })
+
+        const transactionsSummary = await supertest(app.server)
+            .get('/transactions/summary')
+            .set('Cookie', cookies!)
+            .expect(200)
+
+        expect(transactionsSummary.body.summary).toEqual([
+            expect.objectContaining({
+                "sum(`amount`)": 0,
             })
         ])
     })
